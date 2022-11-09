f09d37453bb5ad4342f75f61d68006f4e3d5cf9e
ALEXANDER VARVINSKY <varvinskiyalexander@gmail.com>
2022-11-09 20:25:19 +0300

    homework3

---
+++ homework_03/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/ping")
+def read_root():
+    return {"message": "pong"}
+
+
+@app.get("/{path:path}")
+def all_other(path: str):
+    return {"request to": path}
+
